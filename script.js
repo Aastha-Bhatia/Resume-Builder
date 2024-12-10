4e325c52bff44cf1cf87b1f546ce0f02ad10b22e
Aastha Bhatia <122430152+Aastha-Bhatia@users.noreply.github.com>
2024-12-10 19:04:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -240,8 +240,7 @@ function printresume(){
         alert("Printing is only supported in desktop browsers. Please switch to a desktop for better functionality.");
         return;
     }
-    #btnbtnbtn{
-    display: none;
-  }
+    let printButton = document.getElementById('printResumeButton');
+    printButton.style.display = 'none';
     window.print();
 }
